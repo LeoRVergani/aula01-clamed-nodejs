ff8221fc8680fd442a7c633daf2ffee5708cb61e
leorvergani <vergani@outlook.com.br>
2024-11-18 20:57:39 -0300

    aula semana 02 aula01

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+
+function showFullName(nome: string, sobreNome: string) {
+    return `${nome} ${sobreNome}`
+}
+
+console.log(showFullName(1, 'Vergani'))
+
+
+function somar(valueA: number, valueB: number) {
+    return `${valueA} + ${valueB} = ${valueA + valueA}`
+}
+
+console.log(somar(20, 56))
